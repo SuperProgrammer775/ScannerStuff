d861d97d8b2ce544f8b14847fc254ad6da354bce
Arjun <arjun@anion.com>
2026-09-09 08:38:20 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.io.*;
 
-public class Main {
+class Main {
+    public static void main(String[] args) {
+        int a = 10, b = 15;
+
+        int ans = a*b;
+
+        System.out.println("a times b equals: " + ans);
+    }
 }
